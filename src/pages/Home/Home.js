cb45739046d4c630f3122336309e5d44de39350a
CharlesLe114 <baotoan.forwork@gmail.com>
2021-08-13 10:43:16 +0700

    add newTask input field

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,33 @@
 import React, { Component } from "react";
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import NewTask from "../../components/NewTask/NewTask"
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
 class Home extends Component {
   constructor() {
     super();
     this.state = {};
   }
   render() {
-    return <h1>hello abc</h1>;
+    return (
+      // <Container fluid >
+      //   <Row>
+      //     <Col>
+      //       <NewTask />
+      //     </Col>
+      //   </Row>
+      // </Container>
+
+      <InputGroup className="mb-3">
+      <FormControl
+        placeholder="New Task..."
+        aria-label="Username"
+        aria-describedby="basic-addon1"
+      />
+    </InputGroup>
+    );
   }
 }
 
