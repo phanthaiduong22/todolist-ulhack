4e50a028f00559fd5e319afe7e8c2c6fec7c87fb
CharlesLe114 <baotoan.forwork@gmail.com>
2021-08-14 01:13:57 +0700

    easy fix - update CSS

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -5,12 +5,23 @@ import moment from 'moment';
 import callAPI from "../../utils/apiCaller";
 // import {icons} from "../../modules/icons";
 
-function Task({task }) {
+function Task({task, index}) {
   
 	const duration = (moment.duration(moment(task.due_date).diff(moment())))._data.days // Get duration to due date, calculated in hours
 	const formated_date = moment(task.due_date).format("dddd, MMMM Do YYYY")
-	const is_urgent = (duration < 3 && duration > 0) ? true : false // if the task is due in 3 days -> it's urgent
-	const className = "task" + (task.is_done === 1 ? "-checked" : (is_urgent ? "-urgent":(task.is_importance === 1 ? "-important" : "")))
+	const is_urgent = (duration < 3 && duration > -1) ? true : false // if the task is due in 3 days -> it's urgent
+	console.log(`${task.task_name}:${is_urgent}:${duration}`)
+	let className = "task"
+	if (task.is_done == 1) {
+		className = "task-done"
+	} else if (task.is_importance == 1 && is_urgent){
+		className = "task-urgent-important"
+	} else if (task.is_importance == 1){
+		className = "task-important"
+	}
+	else if (is_urgent){
+		className = "task-urgent"
+	} 
 
 	const username = localStorage.getItem("username"); // Get username from localStorage, for demo purpose
 	// Update the task if done
@@ -35,7 +46,7 @@ function Task({task }) {
 			</div>
 
 			<div>
-				<p> <strong>Important:</strong> {task.is_importance == 1 ? "Yes" : "No"}</p>
+				<p> <strong>Important:</strong> {task.is_importance === 1 ? "Yes" : "No"}</p>
 			</div>
 		  </Popover.Body>
 		</Popover>
@@ -44,34 +55,29 @@ function Task({task }) {
   	return (
 		<>	
 			<section className={className}>
-				<div class="${className} text-gray p-3 mb-2 rounded border border-dark">
+				<div className="${className} text-gray p-3 mb-2 rounded border border-dark">
 					<Container>
-						<Row class="row align-items-center">
+						<Row className="align-items-center">
 							<Col xs={1}>
 								<div class="border-item">
-									{task.task_id}
+									{index+1}
 								</div>
 							</Col>
-							<Col xs={7}>
+							<Col xs={6}>
 								<div class="border-item align-center">
 									{task.task_name}
 								</div>
 							</Col>
-							{/* <Col>
-								<div class="border-item">
-									{task.due_date}
+							<Col xs={3}>
+								<div class="border-item align-center">
+									{formated_date}
 								</div>
 							</Col>
-							<Col xs={1}>
-								<div class="border-item">
-									{task.is_importance == 1 ? "yes" : "no"}
-								</div>
-							</Col> */}
-							<Col xs="1">  
+							{/* <Col xs="1">  
 								<OverlayTrigger trigger="click" placement="bottom" overlay={popover}>
 									<Button variant="outline-primary">INFO</Button>
 								</OverlayTrigger>
-  							</Col>	
+  							</Col>	 */}
 							<Col xs="1"><Button variant="outline-success" onClick={doneOnChange}>Done</Button></Col>
 							<Col xs="1"><Button variant="outline-danger" onClick={deleteOnChange}>Delete</Button></Col>
 						</Row>
