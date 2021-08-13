d70fe9c3ef7ee192b853d0a94ad05e48c724bcb5
Duong Phan <phanthaiduong2000@gmail.com>
2021-08-13 12:53:48 +0700

    add logout

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,26 +1,46 @@
-import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import React, { Component } from "react";
+import { Navbar, Nav, Button } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
-const Navigation = () => {
-  return (
-    <Navbar bg="light" expand="lg">
-      <LinkContainer to="/">
-        <Navbar.Brand>React-Bootstrap</Navbar.Brand>
-      </LinkContainer>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <LinkContainer to="/home">
-            <Nav.Link>Home</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/login">
-            <Nav.Link>Login</Nav.Link>
-          </LinkContainer>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
-};
+class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLogIn: false,
+    };
+  }
+  componentDidMount = () => {
+    const username = localStorage.getItem("username");
+    if (username !== null) this.setState({ isLogIn: true });
+  };
+  handleLogoutClick = () => {
+    localStorage.removeItem("username");
+  };
+  render() {
+    let LogOut = null;
+    if (this.state.isLogIn) {
+      LogOut = <Button onClick={this.handleLogoutClick}>Logout</Button>;
+    }
+    return (
+      <Navbar bg="light" expand="lg">
+        <LinkContainer to="/">
+          <Navbar.Brand>React-Bootstrap</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <LinkContainer to="/home">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+        {LogOut}
+      </Navbar>
+    );
+  }
+}
 
 export default Navigation;
