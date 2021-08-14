f080ac9cde22a6a786cedf21e8a54fc6002d2b8f
Duong Phan <phanthaiduong2000@gmail.com>
2021-08-15 01:26:34 +0700

    fix messags

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,20 +1,16 @@
 import React, { Component } from "react";
-import ReactBootstrap from 'react-bootstrap';
-import Item from "../Item/Item"
+import ReactBootstrap from "react-bootstrap";
+import Item from "../Item/Item";
 
 class ItemList extends Component {
-	constructor(props) {
-	  super(props);
-		console.log("from ItemList")
-		console.log(this.props)
-	//   this.state.messages = this.props.messages
-	//   console.log(this.state.messages)
-	}
-	render() {
-	//   let items = this.state.messages.map((item) => <Item message = {item.message}/>);
-	//   return <h4>{items}</h4>;
-	  return <h4>items</h4>;
-	}
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    let messages = this.props.messages;
+    let items = messages.map((item) => <Item message={item.message} />);
+    return <h4>{items}</h4>;
+  }
 }
 
-export default ItemList
+export default ItemList;
--- src/pages/Mymission/Mymission.js
+++ src/pages/Mymission/Mymission.js
@@ -1,32 +1,24 @@
 import React, { Component } from "react";
 import "react-circular-progressbar/dist/styles.css";
-import { Redirect } from "react-router-dom";
-import { Container, Form } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import callAPI from "../../utils/apiCaller";
-import RoundedTitle from "../../components/RoundedTitle/RoundedTitle";
-import ItemList from "../../components/ItemList/ItemList";
 import Mission from "../../components/Mission/Mission";
-import AddMission from "../../components/AddMission/AddMission"
+import AddMission from "../../components/AddMission/AddMission";
 
 class MissionAndVision extends Component {
   constructor(props) {
     super(props);
     this.state = {
       missions: [],
-      username: ""
+      username: "",
     };
   }
-  handleKeyDownCheckList = (e) => {
-    if (e.keyCode == 13) {
-      let messeage = e.target.value;
-    }
-  };
   componentDidMount = () => {
     const username = localStorage.getItem("username");
     if (username === null) {
       this.setState({ redirect: "/login" });
     } else {
-      this.setState({username})
+      this.setState({ username });
       callAPI(`/missions/${username}`, "GET", {})
         .then((response) => {
           this.setState({ missions: response.data.missions });
@@ -42,13 +34,11 @@ class MissionAndVision extends Component {
       <Container>
         <AddMission />
         {this.state.missions.map((mission) => {
-          return <Mission mission = {mission} username = {this.state.username}/>
-          }
-        )}
+          return <Mission mission={mission} username={this.state.username} />;
+        })}
       </Container>
-    )
+    );
   }
-
 }
 
 export default MissionAndVision;
