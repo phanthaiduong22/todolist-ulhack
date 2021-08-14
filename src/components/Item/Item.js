a1d53998fe350094f686bfe5eb4d45051afd5833
CharlesLe114 <baotoan.forwork@gmail.com>
2021-08-15 02:05:32 +0700

    update item

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,35 +1,74 @@
 import React, { Component } from "react";
-import { Container, Form } from "react-bootstrap";
-class Item extends React.Component {
+import callAPI from "../../utils/apiCaller";
+class Item extends Component {
 	constructor(props) {
-	  super();
-  
-	  this.state = {
-		checked: false,
-	  };
-  
-	  this.handleClick = this.handleClick.bind(this);
+		super();
+
+		this.state = {
+			checked: false,
+		};
+
+		this.handleClick = this.handleClick.bind(this);
 	}
+	componentDidMount = () => {
+		this.setState({
+			checked: this.props.message.is_done,
+		})
+	}
+
 	handleClick(e) {
-	  this.setState({
-		checked: !this.state.checked,
-	  });
+		console.log(e.target.value)
+		let message_id = this.props.message.message_id
+		let username = this.props.username
+		if (e.target.value == 0) {
+			callAPI(`/missions/${username}/messages/${message_id}`, "PUT", {
+				message_id,
+			})
+			.then((response) => {
+				this.setState({
+					checked: true,
+				})
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+		} else {
+			callAPI(`/missions/${username}/messages/${message_id}`, "DELETE", {
+				message_id,
+			})
+			.then((response) => {
+				this.setState({
+					checked: false,
+				})
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+		}
+
 	}
 	render() {
-	  let text = this.state.checked ? (
-		<strike>{this.props.message}</strike>
-	  ) : (
-		this.props.message
-	  );
-	  return (
-		<div className="row">
-		  <div className="col-md-12">
-			<input type="checkbox" onClick={this.handleClick} />
-			&nbsp;{text}
-			<hr />
-		  </div>
-		</div>
-	  );
+		let text = this.state.checked ? (
+			<>
+				<input type="checkbox" value={this.state.checked} onClick={this.handleClick} checked/>
+				&nbsp;
+				<strike>{this.props.message.message}</strike>
+			</>
+		) : (
+			<>
+				<input type="checkbox" value={this.state.checked} onClick={this.handleClick}/>
+				&nbsp;
+				{this.props.message.message}
+			</>
+		);
+		return (
+			<div className="row">
+				<div className="col-md-12">
+					{text}
+					<hr />
+				</div>
+			</div>
+		);
 	}
 }
 
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -8,7 +8,7 @@ class ItemList extends Component {
   }
   render() {
     let messages = this.props.messages;
-    let items = messages.map((item) => <Item message={item.message} />);
+    let items = messages.map((item) => <Item message={item} username={this.props.username}/>);
     return <h4>{items}</h4>;
   }
 }
