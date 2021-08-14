fb35cbf97cf15b3d580caeb35475b47f6142d38c
CharlesLe114 <baotoan.forwork@gmail.com>
2021-08-15 02:16:55 +0700

    udpate tag

--- docker.sh
+++ docker.sh
@@ -1,3 +1,3 @@
 docker build . -t zgoalizt-fe:latest
-docker tag zgoalizt-fe:latest lebaotoan/zgoalizt-fe:latest 
-docker push lebaotoan/zgoalizt-fe:latest 
+docker tag zgoalizt-fe:latest lebaotoan/zgoalizt-fe:v1.0
+docker push lebaotoan/zgoalizt-fe:v1.0
